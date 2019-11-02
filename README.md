e3ce7aa3e063e716d22a10c3992403b95f32183d
Dante Rivera <drivera@drivera-linko.linko.mx>
2019-11-01 20:30:48 -0600

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Commands to install Vagrant 2.2.6 on Debian 10 Buster
---
+++ install-vagrant226-debian10.sh
@@ -0,0 +1,6 @@
+# Download Vagrant from Hashicorp
+wget https://releases.hashicorp.com/vagrant/2.2.6/vagrant_2.2.6_x86_64.deb && \
+sudo chmod +x vagrant_2.2.6_x86_64.deb
+
+# Install Vagrant
+sudo apt install vagrant_2.2.6_x86_64.deb
