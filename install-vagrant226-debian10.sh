ec7c8bc114ffc6b428e5fe9249057680b1cc7742
Dante Rivera <drivera@drivera-linko.linko.mx>
2019-11-01 20:32:43 -0600

    last commit

--- install-vagrant226-debian10.sh
+++ install-vagrant226-debian10.sh
@@ -3,4 +3,4 @@ wget https://releases.hashicorp.com/vagrant/2.2.6/vagrant_2.2.6_x86_64.deb && \
 sudo chmod +x vagrant_2.2.6_x86_64.deb
 
 # Install Vagrant
-sudo apt install vagrant_2.2.6_x86_64.deb
+sudo apt-get install ./vagrant_2.2.6_x86_64.deb
